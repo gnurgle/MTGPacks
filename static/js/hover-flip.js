772622a6642506a520c5b2e944da36c0fbdf5497
gnurgle <lewis.russell.j@gmail.com>
2020-10-31 23:51:38 -0400

    Set view check for pulling new pack

--- static/js/hover-flip.js
+++ static/js/hover-flip.js
@@ -1,15 +1,20 @@
 var clicked = 0;
+var flipped = 0;
 var $items = $('.card_click');
 
 $('.card_click').click(function(){
   $(this).toggleClass('flipped');
   clicked = clicked + 1;
-  if ($items.length == clicked)
+  if (clicked + flipped == 15)
     alert ("Can call New Pack");
 		
 	});
 
 $('.card').one("mouseenter",function(){
   $(this).toggleClass('flipped');
+  flipped = flipped + 1;
+  if (clicked + flipped == 15)
+    alert ("Can call New Pack");
+
 	});
 
