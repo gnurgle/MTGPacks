6bde45aac18ed3258b51fd731ef0e53a22584af5
gnurgle <lewis.russell.j@gmail.com>
2020-11-02 01:32:02 -0500

    Basic theme, Pack can be repulled, /pullPack

--- server.py
+++ server.py
@@ -1,15 +1,24 @@
 import os
 from flask import Flask, render_template
 from makePack import make_m19_pack
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
 
 def index():
-	cards_list = make_m19_pack()
+	return render_template("index.html")
 
-	return render_template("index.html", cards = list(cards_list[1:]), info = list(cards_list[0])) 
+@app.route("/pullPack")
+def pull_pack():
+
+	cards_list = make_m19_pack()
+	return render_template("card_pack.html", cards = list(cards_list[1:]), info = list(cards_list[0])) 
+	
+
 
 if __name__ == "__main__":
 	app.run(debug = True)
--- static/js/hover-flip.js
+++ static/js/hover-flip.js
@@ -2,19 +2,47 @@ var clicked = 0;
 var flipped = 0;
 var $items = $('.card_click');
 
-$('.card_click').click(function(){
+$('.card_click').one('click',function(){
   $(this).toggleClass('flipped');
   clicked = clicked + 1;
-  if (clicked + flipped == 15)
-    alert ("Can call New Pack");
-		
-	});
+});
 
 $('.card').one("mouseenter",function(){
-  $(this).toggleClass('flipped');
+    $(this).toggleClass('flipped');
   flipped = flipped + 1;
-  if (clicked + flipped == 15)
-    alert ("Can call New Pack");
+});
 
-	});
+function reload() {
+  	if (clicked + flipped >= 15)
+		loadlink();
+};
 
+function flip(){
+  $(this).toggleClass('flipped');
+};
+function loadlink(){
+
+    $('#outbox').load('/pullPack' + " #outbox > *");
+	clicked = 0;
+	flipped = 0
+	$(document).on("mouseenter", ".card", function(){
+			var $this=$(this);
+			if ($this.data('mouseentered')){
+			return false;
+			}
+			$this.data('mouseentered',true); 
+			$(this).toggleClass('flipped'); 
+			clicked = clicked + 1; 
+		  });
+	$(document).on('click', '.card_click', function(){
+			var $this=$(this);
+			if ($this.data('mouseentered')){
+			return false;
+			}
+			$this.data('mouseentered',true);
+  		$(this).toggleClass('flipped');
+  		clicked = clicked + 1;
+		});
+
+
+}
